fad2bce5958dc4b0a1315b7c808cbfe719ed0845
popoolahussein <popoolakehindehussein@gmail.com>
2024-06-07 20:38:18 +0100

    fix: ensured the activation of functions in the js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,90 +1,96 @@
 const home = document.getElementById('home');
 const guest = document.getElementById('guest');
 const timerElement = document.getElementById('timer');
-let totalSeconds = 2880;
-
-let timerInterval;
-
-// eslint-disable-next-line no-unused-vars
-function startTimer() {
-  totalSeconds = 0;
-  // eslint-disable-next-line no-use-before-define
-  timerInterval = setInterval(updateTimer, 1000);
-}
+let totalSeconds = 0;
+let timerInterval = null;
 
 function stopTimer() {
   clearInterval(timerInterval);
+  timerInterval = null;
 }
 
 function updateTimer() {
-  // eslint-disable-next-line no-plusplus
-  totalSeconds++;
+  totalSeconds += 1;
 
-  if (totalSeconds > 2880) {
-    clearInterval(timerInterval);
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  if (totalSeconds >= 2880) {
+    stopTimer();
     timerElement.textContent = '00:48:00';
   } else {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
 
-    // eslint-disable-next-line no-use-before-define
     const formattedTime = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
 
     timerElement.textContent = formattedTime;
   }
 }
 
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
+function startTimer() {
+  if (!timerInterval) {
+    timerInterval = setInterval(updateTimer, 1000);
+  }
 }
 
-// eslint-disable-next-line no-unused-vars
 function restartTimer() {
   stopTimer();
   totalSeconds = 0;
   timerElement.textContent = '00:00:00';
+  startTimer();
 }
 
-let count = 0;
-let counts = 0;
+let homeScore = 0;
+let guestScore = 0;
 
-// eslint-disable-next-line no-unused-vars
 function addOne() {
-  count += 1;
-  home.textContent = count;
+  homeScore += 1;
+  home.textContent = homeScore;
 }
-// eslint-disable-next-line no-unused-vars
+
 function addTwo() {
-  count += 2;
-  home.textContent = count;
+  homeScore += 2;
+  home.textContent = homeScore;
 }
-// eslint-disable-next-line no-unused-vars
+
 function addThree() {
-  count += 3;
-  home.textContent = count;
+  homeScore += 3;
+  home.textContent = homeScore;
 }
 
-// eslint-disable-next-line no-unused-vars
 function plusOne() {
-  counts += 1;
-  guest.textContent = counts;
-}
-// eslint-disable-next-line no-unused-vars
-function plusTwo() {
-  counts += 2;
-  guest.textContent = counts;
-}
-// eslint-disable-next-line no-unused-vars
-function plusThree() {
-  counts += 3;
-  guest.textContent = counts;
+  guestScore += 1;
+  guest.textContent = guestScore;
 }
 
-// eslint-disable-next-line no-unused-vars
-function newGame() {
-  count = 0;
-  counts = 0;
-  home.textContent = count;
-  guest.textContent = counts;
+function plusTwo() {
+  guestScore += 2;
+  guest.textContent = guestScore;
 }
+
+function plusThree() {
+  guestScore += 3;
+  guest.textContent = guestScore;
+}
+
+function newGame() {
+  homeScore = 0;
+  guestScore = 0;
+  home.textContent = homeScore;
+  guest.textContent = guestScore;
+  restartTimer();
+}
+
+window.startTimer = startTimer;
+window.stopTimer = stopTimer;
+window.restartTimer = restartTimer;
+window.addOne = addOne;
+window.addTwo = addTwo;
+window.addThree = addThree;
+window.plusOne = plusOne;
+window.plusTwo = plusTwo;
+window.plusThree = plusThree;
+window.newGame = newGame;
